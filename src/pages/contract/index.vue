d1cfeee9bef8f4ee37c54d3b77ad35a677dc4d5b
yunfeng0214 <1558003416@qq.com>
2022-11-17 12:59:05 +0800

    打印合同

--- src/pages/contract/index.vue
+++ src/pages/contract/index.vue
@@ -2,34 +2,49 @@
     <div class="content bg-white-page">
         <c-common-top :isBack="true" title='合同'/>
         <div id="pdfDom">
-            666
-            <div class="box"></div>
+          <van-button type="primary" @click="printPdf" class="printbtn">打印合同</van-button>
+            <iframe :src="contractSrc" frameborder="0" width="100%" height="100%"></iframe>
+
         </div>
-        <van-button type='default' @click="printPdf">打印</van-button>
     </div>
 </template>
 
 <script>
+import api from "@/api/api";
 import {getPdf} from '@/utils/htmlToPdf'
-
 export default {
     data() {
         return {
-            htmlTitle: '合同',
+          contractSrc:null,
         };
     },
-    created() { },
+    created() {
+      this.getcontract()
+    },
     methods: {
-        printPdf(){
+      // 打印合同
+     printPdf(){
             getPdf('合同', 'pdfDom')
         },
+      // 获取合同
+       getcontract() {
+      this.$axios.post(api.self.contract, {}).then(res => {
+        console.log("合同", res);
+        this.contractSrc = res.data.url;
+        // this.banners = res.data.list.map(item => item.image)
+      });
+    },
     },
 };
 </script>
 <style lang="less" scoped>
-.box{
-    width: 30px;
-    height: 30px;
-    background-color: skyblue;
+#pdfDom{
+  width: 100%;
+  height: 100vh;
 }
+.printbtn{
+  // position: absolute;
+  // top: 50px;
+}
+
 </style>
